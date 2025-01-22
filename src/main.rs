f7d15699c92ece56890ead763b55823504956ecb
Nick Cao <nickcao@nichi.co>
2025-01-21 22:49:18 -0500

    maybe

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ pub async fn main() {
     dbg!(&service_url);
 
     let client = ClientBuilder::new(
-        Url::parse(&service_url).unwrap(),
+        Url::parse(&service_url).unwrap().join("twirp").unwrap(),
         twirp::reqwest::Client::default(),
     )
     .with(Bearer {
@@ -78,7 +78,7 @@ pub async fn main() {
             })
             .await
             .unwrap();
-        dbg!(resp);
+        assert!(resp.ok);
     }
 
     dbg!(parsed.claims());
