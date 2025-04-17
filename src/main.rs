11cc2bb9bcc3813b6e1db05be099c60f4f59f22f
Nick Cao <nickcao@nichi.co>
2025-04-16 21:45:40 -0400

    Reduce unwrap

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use axum::{
     body::Body,
     extract::{Path, State},
     http::HeaderMap,
+    response::Result,
     response::{IntoResponse, Redirect},
     routing::get,
     Router,
@@ -45,6 +46,7 @@ impl Middleware for Bearer {
 
 struct AppState {
     client: Client,
+    rclient: reqwest::Client,
     version: String,
 }
 
@@ -53,8 +55,30 @@ async fn upload(
     Path(path): Path<String>,
     headers: HeaderMap,
     body: Body,
-) -> impl IntoResponse {
-    let key = format!("{}-{}", path, rand::rng().next_u64());
+) -> Result<impl IntoResponse> {
+    let key = format!("{}-{}", path, rand::rng().next_u32());
+
+    let content_length: i64 = headers
+        .get(header::CONTENT_LENGTH)
+        .ok_or((
+            StatusCode::BAD_REQUEST,
+            "missing content-length header in request",
+        ))?
+        .to_str()
+        .map_err(|err| {
+            (
+                StatusCode::BAD_REQUEST,
+                format!("invalid content-length header in request: {}", err),
+            )
+        })?
+        .parse()
+        .map_err(|err| {
+            (
+                StatusCode::BAD_REQUEST,
+                format!("invalid content-length header in request: {}", err),
+            )
+        })?;
+
     let resp = state
         .client
         .create_cache_entry(CreateCacheEntryRequest {
@@ -63,65 +87,92 @@ async fn upload(
             ..Default::default()
         })
         .await
-        .unwrap();
+        .map_err(|err| {
+            (
+                StatusCode::IM_A_TEAPOT,
+                format!("unexpected error: {}", err),
+            )
+        })?;
+
     if !resp.ok {
-        return (StatusCode::INTERNAL_SERVER_ERROR, "")
-            .into_response()
-            .into_response();
+        return Ok((
+            StatusCode::IM_A_TEAPOT,
+            format!("failed to create key {}", key),
+        ));
     }
 
-    let res = reqwest::ClientBuilder::default()
-        .build()
-        .unwrap()
+    state
+        .rclient
         .put(&resp.signed_upload_url)
-        .header(
-            header::CONTENT_LENGTH,
-            headers.get(header::CONTENT_LENGTH).unwrap(),
-        )
-        // .headers(headers.clone())
+        .header(header::CONTENT_LENGTH, content_length)
         .header("x-ms-blob-type", "BlockBlob")
         .body(reqwest::Body::wrap_stream(body.into_data_stream()))
         .send()
         .await
-        .unwrap();
+        .map_err(|err| {
+            (
+                StatusCode::IM_A_TEAPOT,
+                format!("unexpected error: {}", err),
+            )
+        })?
+        .error_for_status()
+        .map_err(|err| {
+            (
+                StatusCode::IM_A_TEAPOT,
+                format!("unexpected error: {}", err),
+            )
+        })?;
 
-    // TODO: check
-
-    state
+    let res = state
         .client
         .finalize_cache_entry_upload(FinalizeCacheEntryUploadRequest {
             key: key.clone(),
             version: state.version.clone(),
-            size_bytes: headers
-                .get(header::CONTENT_LENGTH)
-                .map(|size| size.to_str().unwrap().parse().unwrap_or(0))
-                .unwrap_or(0),
+            size_bytes: content_length,
             ..Default::default()
         })
         .await
-        .unwrap();
+        .map_err(|err| {
+            (
+                StatusCode::IM_A_TEAPOT,
+                format!("unexpected error: {}", err),
+            )
+        })?;
 
-    (StatusCode::CREATED, "").into_response()
+    if !res.ok {
+        Ok((
+            StatusCode::IM_A_TEAPOT,
+            format!("key {} creation failed", key),
+        ))
+    } else {
+        Ok((StatusCode::CREATED, format!("key {} created", key)))
+    }
 }
 
 async fn download(
     State(state): State<Arc<AppState>>,
-    Path(path): Path<String>,
-) -> impl IntoResponse {
+    Path(key): Path<String>,
+) -> Result<impl IntoResponse> {
     let resp = state
         .client
         .get_cache_entry_download_url(GetCacheEntryDownloadUrlRequest {
-            key: path.clone(),
-            restore_keys: vec![path.clone()],
+            key: key.clone(),
+            restore_keys: vec![key.clone()],
             version: state.version.clone(),
             ..Default::default()
         })
         .await
-        .unwrap();
+        .map_err(|err| {
+            (
+                StatusCode::IM_A_TEAPOT,
+                format!("unexpected error: {}", err),
+            )
+        })?;
+
     if resp.ok {
-        Redirect::temporary(&resp.signed_download_url).into_response()
+        Ok(Redirect::temporary(&resp.signed_download_url).into_response())
     } else {
-        (StatusCode::NOT_FOUND, "").into_response()
+        Ok((StatusCode::NOT_FOUND, format!("key {} not found", key)).into_response())
     }
 }
 
@@ -148,6 +199,7 @@ pub async fn main() {
         .route("/{*path}", get(download).put(upload))
         .with_state(Arc::new(AppState {
             client,
+            rclient: reqwest::ClientBuilder::default().build().unwrap(),
             version: "87428fc522803d31065e7bce3cf03fe475096631e5e07bbd7a0fde60c4cf25c7".to_string(),
         }));
 
